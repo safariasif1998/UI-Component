113b19f58a1c332ec037e0e4cd003236fa95ad40
Asif Safari <as@nea-x.net>
2026-04-24 11:57:55 +0200

    ref (DoubleCheveronUp) - changed  the code in CheveronUp.

--- src/icons/DoubleChevronUp.tsx
+++ src/icons/DoubleChevronUp.tsx
@@ -16,9 +16,9 @@ export function DoubleChevronUp(props: DoubleChevronUpProps) {
       viewBox={`0 0 ${width} ${height}`}
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
     >
       <path d="m17 11-5-5-5 5" />
